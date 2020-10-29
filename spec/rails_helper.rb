e065b7006824405de17ec7097a7b4029c7c9b976
chr-ge <ace26@protonmail.com>
2020-10-29 13:07:13 -0400

    Added ffaker, factory_bot, shoulda-matchers Gems

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,4 +63,17 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include FactoryBot::Syntax::Methods
+
+  %i[controller view request].each do |type|
+    config.include ::Rails::Controller::Testing::TestProcess, type: type
+    config.include ::Rails::Controller::Testing::TemplateAssertions, type: type
+    config.include ::Rails::Controller::Testing::Integration, type: type
+  end
+end
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
