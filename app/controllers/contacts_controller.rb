2024a2cc0d23a7a7fc9c4f66977a9d40d8244cab
chr-ge <ace26@protonmail.com>
2020-10-31 11:30:08 -0400

    Created set_contact before_action method

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,21 +1,51 @@
 # frozen_string_literal: true
 
 class ContactsController < ApplicationController
+  before_action :set_contact, only: [:show, :edit, :update, :destroy]
+
   def index
-    @contacts = Contact.all
+    @contacts = Contact.order('full_name ASC')
   end
 
-  def show
-    @contact = Contact.find(params[:id])
-  end
+  def show; end
 
   def new
     @contact = Contact.new
   end
 
+  def create
+    @contact = Contact.new(contact_params)
+
+    if @contact.save
+      redirect_to contacts_path, notice: 'Contact created successfully!'
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @contact.update(contact_params)
+      redirect_to @contact, notice: "Contact updated successfully."
+    else
+      render :edit
+    end
+  end
+
   def destroy
-    contact = Contact.find(params[:id])
-    contact.destroy
+    @contact.destroy
     redirect_to contacts_path, notice: 'Contact deleted successfully!'
+  end
+
+  private
+
+  def set_contact
+    @contact = Contact.find(params[:id])
+  end
+
+  def contact_params
+    params.require(:contact)
+          .permit(:full_name, :organization, :notes, :emails => [:label, :email])
   end
 end
