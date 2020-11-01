3a49e47fd106d24c00c1de266d021c3c69b59922
chr-ge <ace26@protonmail.com>
2020-11-01 01:07:14 -0400

    Added accepts_nested_attributes_for

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 class Contact < ApplicationRecord
+  has_many :emails, inverse_of: :contact, dependent: :destroy
+  accepts_nested_attributes_for :emails, reject_if: :all_blank, allow_destroy: true
+
   validates :full_name, presence: true
 end
