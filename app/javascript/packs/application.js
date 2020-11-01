74b08e83080cf0e6a21206ba3b75dd57032614df
chr-ge <ace26@protonmail.com>
2020-11-01 14:24:12 -0500

    Added cocoon Gem

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require('@kollegorna/cocoon-vanilla-js')
 
 import "../stylesheets/application.scss"
 
