d18930287288f2699affb2a8bb28d047c06caeb1
chr-ge <ace26@protonmail.com>
2020-11-02 10:05:12 -0500

    Added pg_search_scope

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Contact < ApplicationRecord
+  include PgSearch::Model
+
   has_many :emails, inverse_of: :contact, dependent: :destroy
   has_many :phone_numbers, inverse_of: :contact, dependent: :destroy
   accepts_nested_attributes_for :emails, allow_destroy: true, reject_if: proc { |att| att['email'].blank? }
@@ -8,4 +10,11 @@ class Contact < ApplicationRecord
     proc { |att| att['phone_number'].blank? }
 
   validates :full_name, presence: true
+  pg_search_scope :search_for, 
+    associated_against: {
+      emails: %i( email ),
+      phone_numbers: %i( phone_number )
+    },
+    against: %i(full_name organization notes),
+    order_within_rank: "contacts.full_name ASC"
 end
