a40e3a4ae4aa24dcdea3288000286a323fe9a42f
chr-ge <ace26@protonmail.com>
2020-11-03 19:50:37 -0500

    Style & Formatting

--- spec/factories/relationships.rb
+++ spec/factories/relationships.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :relationship do
     tag { Relationship::TAGS.sample }
-    
+
     association :person, factory: :contact
     association :contact, factory: :contact
   end
