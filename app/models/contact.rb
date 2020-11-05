cc4062848f5e9a0d4124baf63a7cf68fad98eb08
chr-ge <ace26@protonmail.com>
2020-11-05 17:49:53 -0500

    accepts_nested_attributes to reject all_blank

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -7,10 +7,9 @@ class Contact < ApplicationRecord
   has_many :phone_numbers, inverse_of: :contact, dependent: :destroy
   has_many :relationships, inverse_of: :contact, dependent: :destroy
 
-  accepts_nested_attributes_for :emails, allow_destroy: true, reject_if: proc { |att| att['email'].blank? }
-  accepts_nested_attributes_for :phone_numbers, allow_destroy: true, reject_if:
-    proc { |att| att['phone_number'].blank? }
-  accepts_nested_attributes_for :relationships, allow_destroy: true, reject_if: proc { |att| att['tag'].blank? }
+  accepts_nested_attributes_for :emails, allow_destroy: true, reject_if: :all_blank
+  accepts_nested_attributes_for :phone_numbers, allow_destroy: true, reject_if: :all_blank
+  accepts_nested_attributes_for :relationships, allow_destroy: true, reject_if: :all_blank
 
   validates :full_name, presence: true
 
