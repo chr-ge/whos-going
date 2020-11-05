fa75bc58f415743a06769b25dff537bb08e05f47
chr-ge <ace26@protonmail.com>
2020-11-04 20:14:26 -0500

    Fixed Event Factory (date)

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :event do
     name { FFaker::CheesyLingo.title }
-    date { FFaker::Time.datetime }
+    date { FFaker::Time.between(Time.now, 3.years.from_now) }
     attendees { rand 0..500 }
   end
 end
