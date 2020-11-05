e30825ecf886e2832a7d7e213dba8dffd6b7fc86
chr-ge <ace26@protonmail.com>
2020-11-04 22:22:13 -0500

    Ordering Events by Date

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,7 @@ class EventsController < ApplicationController
   before_action :set_event, only: %i[show edit update destroy]
 
   def index
-    @events = Event.all
+    @events = Event.order('date')
   end
 
   def show
